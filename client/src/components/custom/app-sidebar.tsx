ea859a8f04a112a8a1b88dd22c8336d0590e88f2
ShaswataG <gshaswata1@gmail.com>
2025-05-24 00:22:20 +0530

    style: highlight current document in sidebar

--- client/src/components/custom/app-sidebar.tsx
+++ client/src/components/custom/app-sidebar.tsx
@@ -12,37 +12,11 @@ import {
 } from "@/components/ui/sidebar"
 
 import { useDocumentStore } from "@/stores/documentStore"
-
-// Menu items.
-const items = [
-  {
-    title: "Computer Networks",
-    url: "1",
-    icon: Home,
-  },
-  {
-    title: "Compiler Design",
-    url: "2",
-    icon: Inbox,
-  },
-  {
-    title: "Database Management System",
-    url: "3",
-    icon: Calendar,
-  },
-  {
-    title: "Operating System",
-    url: "4",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "5",
-    icon: Settings,
-  },
-]
+import { useParams } from "react-router-dom"
 
 export function AppSidebar() {
+
+  const { docId } = useParams()
 
   const { documentsByUser:items } = useDocumentStore()
 
@@ -55,7 +29,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.id}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild className={`${item.id === docId ? 'bg-[#949494] text-[white] ' : ''}`}>
                     <a href={item.id}>
                       <span>{item.filename}</span>
                     </a>
