b749c1b4863da800bf732c7670b50d7ab81daf76
ShaswataG <gshaswata1@gmail.com>
2025-05-23 19:57:35 +0530

    refactor: remove print statements

--- server/app/llm_utils.py
+++ server/app/llm_utils.py
@@ -86,12 +86,8 @@ def get_answer_once(document_id: str, document_text: str, question: str) -> str:
     if document_id not in index_cache:
         # print('document_id not in index_cache')
         build_index_from_text(document_id, document_text)
-    print('document_id in index_cache')
     # Get context
     context = get_context_from_index(document_id, question)
-
-    print('context: ', context)
-    print('question: ', question)
 
 
     llm = TogetherLLM(
